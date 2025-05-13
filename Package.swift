88b6167c4150aca1ef31ee921008a104129b092c
Jie <weejie_chen@icloud.com>
2025-05-13 10:17:06 +0800

    同时支持 Swift 和 OC

--- Package.swift
+++ Package.swift
@@ -8,15 +8,25 @@ let package = Package(
     platforms: [.iOS(.v12)],
     products: [
         .library(
+            name: "WJHelper",
+            targets: ["WJHelper"]),
+        .library(
             name: "WJHelper_Objc",
             targets: ["WJHelper_Objc"]),
     ],
     targets: [
         .target(
+            name: "WJHelper",
+            // 假如 Swift 库依赖本地 WJHelper_Objc 库，即在 dependencies 中配置
+            dependencies: ["WJHelper_Objc"],
+            path: "Source/WJHelper",
+            swiftSettings: [.define("SPM_MODE")]
+        ),
+        .target(
             name: "WJHelper_Objc",
             path: "Source/WJHelper_Objc",
             publicHeadersPath: ""
-        ),
+        )
     ]
 )
 
---
+++ Source/WJHelper/Math.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  WJHelper
+//
+//  Created by FuDai on 2025/5/13.
+//
+
+import Foundation
+
+public class Math {
+    
+    static func add(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+}
