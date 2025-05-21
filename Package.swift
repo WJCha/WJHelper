e43b8adb5f2d43c8d29a371779007a8990be90fb
Jie <weejie_chen@icloud.com>
2025-05-21 15:32:10 +0800

    移除测试

--- Package.swift
+++ Package.swift
@@ -7,22 +7,12 @@ let package = Package(
     name: "WJHelper",
     platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "WJHelper",
-            targets: ["WJHelper"]),
         .library(
             name: "WJHeaderPageView",
             targets: ["WJHeaderPageView"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WJHeaderPageView", path: "Sources/WJHeaderPageView"),
-        .testTarget(
-            name: "WJHelperTests",
-            dependencies: ["WJHelper"]
-        ),
+            name: "WJHeaderPageView", path: "Sources/WJHeaderPageView")
     ]
 )
